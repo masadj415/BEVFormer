d314ffe19a0ca473f7f912a07c5afe998dfc2841
masadj415 <masadjuric.djuric@gmail.com>
2026-05-28 15:43:04 +0200

    Update

--- projects/mmdet3d_plugin/bevformer/detectors/__init__.py
+++ projects/mmdet3d_plugin/bevformer/detectors/__init__.py
@@ -2,3 +2,4 @@ from .bevformer import BEVFormer
 from .bevformer_fp16 import BEVFormer_fp16
 from .bevformerV2 import BEVFormerV2
 from .bevformer_vjepa import BEVFormerVJepa
+from .bevformer_dino import BEVFormerDino
